829a4ceb74a2c09121fa02096cf68bc8781b7afe
Sansone11 <sansonemirco@gmail.com>
2022-07-22 15:59:49 +0200

    coding-edit

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ console.log('JS ok')
 // stampare fizz al posto del numero
 // se i è multiplo di 5
 // stampare buzz al posto del numero
-// e se i è multiplo di 3 che di 5
-// dai istruzione di stampare fizzbuzzbuzz al posto del numero
+// e se i è multiplo di 3 e multiplo di 5
+// dai istruzione di stampare fizzbuzz al posto del numero
 for (let i = 1 ; i <= 100; i++){
     console.log(i);
     if (((i + 1) % 3) === 0) {
@@ -14,4 +14,10 @@ for (let i = 1 ; i <= 100; i++){
     if (((i + 1) % 5) === 0) {
         console.log('questo è un multiplo di 5');
     }
+    if (((i + 1) % 3) === 0) and (((i + 1) % 5) === 0);
+        
+    else {
+        console.log('fizzbuzz ');
+    } 
+    
 }
