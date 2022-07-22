853788094347445fe6b000ac5bf3fe3f19ce737e
Sansone11 <sansonemirco@gmail.com>
2022-07-22 17:26:14 +0200

    code-sintax-review

--- js/app.js
+++ js/app.js
@@ -7,18 +7,15 @@ console.log('JS ok')
 // e se i è multiplo di 3 e multiplo di 5
 // dai istruzione di stampare fizzbuzz al posto del numero
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
-    if (((i + 1) % 3) === 0) {
-        console.log('questo è un multiplo di 3');
+    console.log(i)
+    if ((i % 3) === 0) {
+        console.log('questo è un multiplo di 3')
     }
-    if (((i + 1) % 5) === 0) {
-        console.log('questo è un multiplo di 5');
+    if ((i % 5) === 0) {
+        console.log('questo è un multiplo di 5')
     }
-    if (((i + 1) % 3) === 0); and(((i + 1) % 5) === 0); {
-
+    if (((i % 3) === 0) && ((i % 5) === 0)) {
+        console.log('fizzbuzz ')
     }     
-    else {
-        console.log('fizzbuzz ');
-    }
 
 }
