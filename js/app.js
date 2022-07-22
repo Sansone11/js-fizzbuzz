363638af142f12ade2e59e5423a7393f126706e2
Sansone11 <sansonemirco@gmail.com>
2022-07-22 20:28:30 +0200

    dom-edit

--- js/app.js
+++ js/app.js
@@ -6,25 +6,32 @@ console.log('JS ok')
 // stampare buzz al posto del numero
 // e se i è multiplo di 3 e multiplo di 5
 // dai istruzione di stampare fizzbuzz al posto del numero
-// crea un collegamento con il dom querySelector
+// crea un collegamento con il dom querySelector richiama una classe
 const grigliaElement = document.querySelector('.griglia');
-console.log(grigliaElement)
-grigliaElement.innerHTML = ''
+console.log(grigliaElement);
+const divElement = document.createElement('div');
+divElement.classList.add('cell');
 
 for (let i = 1; i <= 100; i++) {
     console.log(i)
     if (i % 3 === 0){
-        const divElement = document.createElement('div')
-        divElement.append(i)
-        divElement.classList.add('cella')
-    
+        console.log('fizz')
+        divElement.append('fizz')
+        divElement.classList.add('fizz')  
     }
     if (i % 5 === 0){
         console.log('buzz')
+        divElement.classList.add('buzz')
+        divElement.append('buz')
+        
     }
     if (i % 3 === 0 && i % 5 === 0){
-        console.log('fizzbuzz ')
-    }     
+        console.log('fizzbuzz')
+        divElement.append('fizzbuzz')
+        divElement.classList.add('fizzbuz')
+        
+    } 
+    grigliaElement.append(divElement);   
 
 }
 
